b042f570017186d3cb72fdf43993d93bc9b1d41d
Jason Howie <howiejason93@gmail.com>
2019-06-18 15:26:54 -0400

    color randomizer

--- js/script.js
+++ js/script.js
@@ -77,26 +77,43 @@ let quotes = [
   }
 ];
 
+// Hex colors in array to switch randomly. Found at: https://gist.github.com/mucar/3898821
+let colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+      '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+      '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+      '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+      '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+      '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+      '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+      '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+      '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+      '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 
 /*
  * Gets a random number betweet 0 and the length of the quote array.
  * returns the random quote selected. 
  */
 function getRandomQuote(arr) {
-  let randomNum = Math.floor(Math.random() * arr.length) + 0
-  return quotes[randomNum];
+  let randomNum = Math.floor(Math.random() * arr.length) + 0;
+  return arr[randomNum];
 }
 
-
+// Does same but with the color array
+function getRandomColor(arr) {
+  let randomNum = Math.floor(Math.random() * arr.length) + 0;
+  return arr[randomNum];
+}
 
 /*
  * Takes the random quote and puts the data from the object into a string
  * If there is a citation and year for the quote it'll add the HTML.
- * Then takes the DOM and adds the string to the HTML file. 
+ * Then takes the DOM and adds the string to the HTML file.
+ * Also changes the background color 
  */
 
 function printQuote() {
   let randomQuote = getRandomQuote(quotes);
+  let randomColor = getRandomColor(colorArray);
   let HTMLString = `<p class="quote">${randomQuote.quote}</p>
   <p class="source">${randomQuote.source}`
 
@@ -111,6 +128,7 @@ function printQuote() {
   HTMLString += `</p>`;
 
   document.getElementById('quote-box').innerHTML = HTMLString;
+  document.querySelector('body').style.backgroundColor = randomColor;
 }
 
 printQuote();
